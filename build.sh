7dadd285a35b8c83f01b74635105f1741c7ae8be
khalid <eternalkhalidkhnz@gmail.com>
2025-01-25 17:35:29 +0530

    DEPLOY:BUILD

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
+echo GO_BLOG
 cd ./go-blog
 make build
 
+echo GO_ECOM
 cd ../go-ecom
 make build
 
+echo GO_GATEWAY
 cd ../go-gateway
 make build
 
+echo GO_SASS
 cd ../go-sass
 make build
+
+cd ..
+ls
+
+git add .
+git commit -m "DEPLOY:BUILD"
