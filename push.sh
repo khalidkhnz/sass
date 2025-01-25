93125addbf0ae705d97e0e340f3fcd1b656e5b90
khalid <eternalkhalidkhnz@gmail.com>
2025-01-25 18:28:13 +0530

    Push

--- push.sh
+++ push.sh
@@ -1,3 +1,26 @@
+echo GO_BLOG
+cd ./go-blog
+GOOS=linux GOARCH=amd64 go build -o bin/backend .
+
+
+echo GO_ECOM
+cd ../go-ecom
+GOOS=linux GOARCH=amd64 go build -o bin/backend .
+
+
+echo GO_GATEWAY
+cd ../go-gateway
+GOOS=linux GOARCH=amd64 go build -o bin/backend .
+
+
+echo GO_SASS
+cd ../go-sass
+GOOS=linux GOARCH=amd64 go build -o bin/backend .
+
+
+cd ..
+
+
 git add .
 git commit -m "Push"
 
